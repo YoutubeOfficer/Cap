e5aa56a883e4aa5f9d69902eb29cae73779f2d81
kimak <kimak@AIUEO>
2022-05-17 21:02:35 +0900

    JDBC 테스트

---
+++ Cap/src/main/java/com/cap/program/Main1.java
@@ -0,0 +1,12 @@
+package com.cap.program;
+
+import java.sql.SQLException;
+
+public class Main1 {
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		JDBCTest ob= new JDBCTest();
+		ob.selectEmp();
+	}
+
+}
